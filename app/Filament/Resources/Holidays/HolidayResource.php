f37f24f8e60d89c76b196345decff574ab25cb1d
Ricardo Gauto <ricardogauto11@gmail.com>
2026-04-29 00:53:22 -0300

    Update navigation icon for HolidayResource from OutlinedRectangleStack to CalendarDateRange for improved visual representation in the portal interface.

--- app/Filament/Resources/Holidays/HolidayResource.php
+++ app/Filament/Resources/Holidays/HolidayResource.php
@@ -18,7 +18,7 @@ class HolidayResource extends Resource
 {
     protected static ?string $model = Holiday::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::CalendarDateRange;
 
     protected static ?string $recordTitleAttribute = 'name';
 
