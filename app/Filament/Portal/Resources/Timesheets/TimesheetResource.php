f9bd88ae473598b79b5aa07fbfb048b522e56838
Ricardo Gauto <ricardogauto11@gmail.com>
2026-04-30 03:47:46 -0300

    Enhance Timesheet query to order results by ID in descending order for better data retrieval.

--- app/Filament/Portal/Resources/Timesheets/TimesheetResource.php
+++ app/Filament/Portal/Resources/Timesheets/TimesheetResource.php
@@ -52,6 +52,8 @@ class TimesheetResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()->where('user_id', Auth::user()->id);
+        return parent::getEloquentQuery()
+            ->where('user_id', Auth::user()->id)
+            ->orderBy('id', 'desc');
     }
 }
