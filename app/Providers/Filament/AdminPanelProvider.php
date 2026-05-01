8f86314fdce3dadfca58b854a041d2a4d793b6ae
Ricardo Gauto <ricardogauto11@gmail.com>
2026-05-01 18:32:58 -0300

    Add database notifications to AdminPanelProvider for enhanced user feedback on resource edits.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,6 +56,7 @@ class AdminPanelProvider extends PanelProvider
             // ->sidebarFullyCollapsibleOnDesktop();
             ->sidebarCollapsibleOnDesktop()
             ->resourceCreatePageRedirect('index')
-            ->resourceEditPageRedirect('index');
+            ->resourceEditPageRedirect('index')
+            ->databaseNotifications();
     }
 }
