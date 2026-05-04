35757217be441493ae7f0f2086b2d72d739c114c
Ricardo Gauto <ricardogauto11@gmail.com>
2026-05-04 10:37:07 -0300

    Add CheckAdminPanelAccess middleware to AdminPanelProvider for enforcing access control in the admin panel.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Http\Middleware\CheckAdminPanelAccess;
 use Filament\Actions\Action;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Enums\UserMenuPosition;
@@ -53,6 +54,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                CheckAdminPanelAccess::class,
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
